44d2e80baf95f737e2eb32b57a83f22754831942
Hussein Omar <husseinomar6190@gmail.com>
2022-06-28 22:42:36 +0300

    saving notes to users broswer localstorage

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -4,7 +4,7 @@ import Showdown from 'showdown';
 import 'react-mde/lib/styles/css/react-mde-all.css';
 
 export default function Editor({ currentNote, updateNote }) {
-  const [selectedTab, setSelectedTab] = React.useState('write');
+  const [selectedTab, setSelectedTab] = React.useState('preview');
 
   const converter = new Showdown.Converter({
     tables: true,
